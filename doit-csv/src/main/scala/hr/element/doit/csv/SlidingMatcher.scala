8ed128dabc5fec569aea04e6647ed1a1ffbc7303
Marin Vila <rinmalavi@gmail.com>
2012-02-14 14:37:22 +0100

    working alpha

--- doit-csv/src/main/scala/hr/element/doit/csv/SlidingMatcher.scala
+++ doit-csv/src/main/scala/hr/element/doit/csv/SlidingMatcher.scala
@@ -3,7 +3,6 @@ package hr.element.doit.csv
 
 
 object SlidingMatcher {
-//  type MatcherResult = Either[Unit, Option[Char]]
 
  /* def apply(delimiter: Char): SlidingMatcher =
     apply(Array(delimiter))
@@ -58,14 +57,10 @@ class SingleCharacterMatcher(
       case Adelimiter => Delimiter
       case AnewLine   => NewLine
       case x => Ch3(x.head)
-
 }
   def flush(): Array[Char] =
     Array.empty
-
 }
-
-
 
 import scala.annotation.tailrec
 
@@ -100,9 +95,9 @@ class CyclicCharacterMatcher(
             last.drop(head-1) ++ last.take(tail)
             else
                 last.drop(head).take(length)
-         println("r:"+readPoint+"c"+buffTake+":"+ new String(sub) + ":" + new String(revolver((length + readPoint) % length)))
+         //println("r:"+readPoint+"c"+buffTake+":"+ new String(sub) + ":" + new String(revolver((length + readPoint) % length)))
       sub sameElements
-         revolver((length + readPoint + 1) % length)  //todo
+         revolver((length + readPoint + 1) % length)
       }
     }
   }
@@ -117,7 +112,6 @@ class CyclicCharacterMatcher(
       clone()
 
    var writePoint = 0
-      // ()
   def consume(read: Char, mode: ( Smr => ModeCase ) )= {    //  should return error if buffer is not empty after special cases
     val modedMatchers = rH.filter(x => mode(x.matchMsg) != Ignore)
     val bufflength = modedMatchers.maxBy(_.length).length
@@ -158,101 +152,8 @@ class CyclicCharacterMatcher(
             else
                 last.drop(head).take(tail - head)
 
-//
-//    if (buffTake < last.length) {
-//
-//      val readPoint = buffTake
-//      buffTake = 0
-//      last.take(readPoint)
-//    } else {
-//      println("f:"+buffTake)
-//      val readWritePoint = buffTake % last.length
-//      buffTake = 0;
-//      last.takeRight(last.length - readWritePoint) ++ last.take(readWritePoint)
-//    }
+
   }
 }
 
 
-//    //@tailrec
-//    if (quotedMode) {           // quotes mode uses only first qoute.length of buffer
-//      val readWritePoint = (cooldown - 1) % quote.length
-//      if (cooldown < quote.length){
-//        last(readWritePoint) = read
-//        Cooldown
-//      } else {      //  quotes mode  cooldown > qoutes length
-//        if (qr.is) {  // qoutes
-//          cooldown -= quote.length
-//          Quote
-//        }
-//        else {    //    no quotes, no cooldown return char
-//          val ret= last((readWritePoint + 1) % quote.length)
-//          last(readWritepoint) = read
-//          Ch3(ret)
-//        }
-//      }                   //   end of quotesmode
-//    }
-//    else {
-//      val readWritePoint = (cooldown - 1) % quote.length
-//      last(readWritePoint) = read
-//      if (rH(0).is){
-//          rH(0).matchMsg
-//      }
-//      else {
-//        if (rH(1).is){
-//          rH(1).matchMsg
-//        }
-//        else {
-//          if(rH(2).is) {
-//            rH(2).matchMsg
-//          }
-//          else {
-//            if(last.length < cooldown) {
-//              Cooldown
-//            }
-//            else {
-//              Ch3(last((readWritePoint + 1) % quotes.length))
-//            }
-//          }
-//        }
-//      }
-//    }
-//  }
-
-/*class TwoCharacterMatcher(head: Char, tail: Char) extends SlidingMatcher {
-  var last: Option[Char] = None
-  def consume(read: Char, qoutedMode: Boolean) =
-    last match {
-      case None =>
-        last = Some(read)
-        Cooldown
-
-      case Some(ch) if (ch == head) && (read == tail) =>
-        last = None
-        Delimiter
-
-      case _ =>
-        val res = last
-        last = Some(read)
-
-        res.map(Ch3(_)).getOrElse(Cooldown)
-    }
-
-  def flush(): Array[Char] =
-    last.toArray
-
-}*/
-//    if (cooldown < delimiter.length) {
-//      last(readWritePoint) = read
-//      Cooldown
-//    } else {
-//      val res = last((readWritePoint + 1) % delimiter.length)
-//      last(readWritePoint) = read;
-//      if (last sameElements
-//        cyclic((delimiter.length - readWritePoint) % delimiter.length)) {
-//        cooldown = 0;
-//        Delimiter
-//      } else {
-//        Ch3(res)
-//      }
-//    }
